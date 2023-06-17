b5536340783e2bd1a5947452934bd2964c647ea9
[Luna Shrestha] <[luna.shrestha@mnsu.edu]>
2023-06-16 22:37:03 -0500

    multiple calls asyn using fork JOin now

--- src/app/components/multiple-services/multiple-services.component.ts
+++ src/app/components/multiple-services/multiple-services.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CountryServiceService } from 'src/app/services/country-service.service';
 import { CustomerServiceService } from 'src/app/services/customer-service.service';
-
+import { forkJoin } from 'rxjs';
 @Component({
   selector: 'app-multiple-services',
   templateUrl: './multiple-services.component.html',
@@ -13,8 +13,11 @@ export class MultipleServicesComponent implements OnInit {
   constructor(private _country : CountryServiceService, private _customer :CustomerServiceService) { }
 
   ngOnInit(): void {
-    this._country.getCountries().subscribe((response:any) => this.countryResponse = response);
-    this._customer.getCustomers().subscribe((response:any)=>this.customerResponse = response);
-  }
+    forkJoin([this._country.getCountries(), this._customer.getCustomers()]).subscribe((response: any)=>{
+      this.countryResponse = response[0];
+      this.customerResponse = response[1];
+    })
+    }
+    
 
 }
